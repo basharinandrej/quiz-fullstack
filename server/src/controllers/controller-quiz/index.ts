c0471fc445490df8952a289ad9d0ac4a892a0e37
Basharin Andrey <a.basharin@okko.tv>
2023-01-08 17:27:06 +0300

    added error handler for controller quiz

--- server/src/controllers/controller-quiz/index.ts
+++ server/src/controllers/controller-quiz/index.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from "express";
 import {serviceQuiz} from '#services/service-quiz'
 import { IRequestQuizAll } from './types'
+import { ApiError } from "#middlewaresapi-error-middleware";
 
 class ControllerQuiz {
-    async quizAll(req: IRequestQuizAll, res: Response) {
+    async quizAll(req: IRequestQuizAll, res: Response, next: (err: ApiError) => void) {
         try {
             serviceQuiz.getQuizzesAll(req, res)
         } catch (error) {
-            res.send(error)
+            if(error instanceof Error) {
+                next(ApiError.internal(error.message))
+            }
         }
     }
-    async create(req: Request, res: Response) {      
+    async create(req: Request, res: Response, next: (err: ApiError) => void) {      
         try {
             serviceQuiz.createQuiz(req, res)
         } catch (error) {
-            res.send(error)
+            if(error instanceof Error) {
+                next(ApiError.internal(error.message))
+            }
         }
     }
 }
